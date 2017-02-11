5912651fd7b8e218dff06786a734abbde04a215a
Li Zhicong <lizhicong@huobi.com>
2017-02-11 18:57:09 +0800

    add constants file

---
+++ trader/constants.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# 交易所
+EXCHANGE_HUOBI = 'HUOBI'  # 火币交易所
+EXCHANGE_OKCOIN = 'OKCOIN'  # okcoin交易所
+
+# 币种
+BTC = 'BTC'  # 比特币
+LTC = 'LTC'  # 莱特币
+
+# 类型
+SPOT = 'SPOT'  # 现货
+FUTURE_WEEK = 'FUTURE_WEEK'  # 期货周合约
+FUTURE_NEXT_WEEK = 'FUTURE_NEXT_WEEK'  # 期货次周合约
+FUTURE_SEASON = 'FUTURE_SEASON'  # 期货季度合约
+
+# 货币类型
+CNY = 'CNY'  # 人民币
+USD = 'USD'  # 美元
